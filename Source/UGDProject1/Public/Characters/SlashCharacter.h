993da6559b7fe867d49fcde9647a22fa3ba5c323
coubs <john.mccoubrie15@gmail.com>
2023-05-22 10:47:34 -0400

    GameModeUpdate

--- Source/UGDProject1/Public/Characters/SlashCharacter.h
+++ Source/UGDProject1/Public/Characters/SlashCharacter.h
@@ -12,18 +12,12 @@ class UGDPROJECT1_API ASlashCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	ASlashCharacter();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+protected:
+	virtual void BeginPlay() override;
+protected:
+	void MoveForward(float Value);
 
 };
