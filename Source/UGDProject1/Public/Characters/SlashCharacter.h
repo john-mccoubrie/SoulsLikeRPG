13b87f7b7a285f449a24bc0135367c46cf5f9291
coubs <john.mccoubrie15@gmail.com>
2023-05-26 10:17:50 -0400

    Attack animation montage

--- Source/UGDProject1/Public/Characters/SlashCharacter.h
+++ Source/UGDProject1/Public/Characters/SlashCharacter.h
@@ -11,6 +11,7 @@ class USpringArmComponent;
 class UCameraComponent;
 class UGroomComponent;
 class AItem;
+class UAnimMontage;
 
 UCLASS()
 class UGDPROJECT1_API ASlashCharacter : public ACharacter
@@ -28,6 +29,7 @@ protected:
 	void Turn(float Value);
 	void LookUp(float Value);
 	void EKeyPressed();
+	void Attack();
 private:
 
 	ECharacterState CharacterState = ECharacterState::ECS_Unequipped;
@@ -49,6 +51,11 @@ private:
 
 	UPROPERTY(VisibleInstanceOnly)
 	AItem* OverlappingItem;
+
+	//Animation montages
+
+	UPROPERTY(EditDefaultsOnly, Category = "Montages")
+	UAnimMontage* AttackMontage;
 
 public:
 	FORCEINLINE void SetOverlappingItem(AItem* Item) { OverlappingItem = Item; }
