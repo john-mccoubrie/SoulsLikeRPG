fd6a30fd05e8593be13cb0d1a4b33ee51c7054f0
coubs <john.mccoubrie15@gmail.com>
2023-05-27 08:23:15 -0400

    Character updates

--- Source/UGDProject1/Public/Characters/CharacterTypes.h
+++ Source/UGDProject1/Public/Characters/CharacterTypes.h
@@ -8,3 +8,9 @@ enum class ECharacterState : uint8
 	ECS_EquippedOneHandedWeapon UMETA(DisplayName = "Equipped One-Handed Weapon"),
 	ECS_EquippedTwoHandedWeapon UMETA(DisplayName = "Equipped Two-Handed Weapon")
 };
+
+enum class EActionState : uint8
+{
+	EAS_Unoccupied UMETA(Display = "Unoccupied"),
+	EAS_Attacking UMETA(Display = "Attacking")
+};
