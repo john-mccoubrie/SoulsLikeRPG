a7d4932945c26cee4077e3226f9610e2e376c37e
coubs <john.mccoubrie15@gmail.com>
2023-06-05 09:50:08 -0400

    More breakable actors and sounds

--- Source/UGDProject1/UGDProject1.Build.cs
+++ Source/UGDProject1/UGDProject1.Build.cs
@@ -8,7 +8,7 @@ public class UGDProject1 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HairStrandsCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HairStrandsCore", "GeometryCollectionEngine" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
